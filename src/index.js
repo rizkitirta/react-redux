5142f4ca6a1c294f2ed2f25ab9fd6f67ebb46ead
rizkitirta <rizkibuwono0@gmail.com>
2021-11-16 14:45:49 +0700

    setup

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore,compose,applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'   
+import reducer from './reducers'
+
+const store = createStore(reducer, compose(applyMiddleware(thunk )))
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reducers/kontak/index.js
@@ -0,0 +1,13 @@
+const  initialState = {
+    
+}
+
+const kontak = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+            break;
+    }
+}
+
+export default kontak;
